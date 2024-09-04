c22184d4891acd7a1b3ddc85e8ea2143d97a4db8
vivekNinjatech <vivek@ninjatechnolabs.com>
2024-09-04 11:24:45 +0530

    learn: few difficult problems

--- STRIVER/video/vid-5/pattern17/p17.cpp
+++ STRIVER/video/vid-5/pattern17/p17.cpp
@@ -1,8 +1,8 @@
-// A
-// BB
-// CCC
-// DDDD
-// EEEEE
+//    A
+//   ABA
+//  ABCBA
+// ABCDCBA
+
 
 #include <iostream>
 using namespace std;
@@ -11,13 +11,28 @@ int main()
 {
     int row = 4;
 
-    for (char i = 'A'; i <= row + 'A'; i++)
-    {
-        for (char j = 'A'; j <= i; j++)
-        {
-            cout << i;
+    for (char i = 0; i < row; i++){
+
+        // space
+        for(int j=0; j< row-i-1; j++){
+            cout<<"-";
         }
-        cout << endl;
+
+        // stuff
+        char ch = 'A';
+        int brk = ((2*i)+1)/2;
+        for(int j=1; j<= (2*i)+1; j++){
+            cout<<ch;
+            if(j<=brk) ch++;
+            else ch--;
+        }        
+
+        // space
+        for(int j=0; j< row-i-1; j++){
+            cout<<"-";
+        }
+
+        cout<<endl;
     }
 
     return 0;
--- STRIVER/video/vid-5/pattern17/p17.py
+++ STRIVER/video/vid-5/pattern17/p17.py
@@ -1,15 +1,60 @@
-# ABCDE
-# ABCD
-# ABC
-# AB
-# A
+#    A
+#   ABA
+#  ABCBA
+# ABCDCBA
 
-def p15(row:int)->None:
+def p17(row:int)->None:
 
-    for i in range(row,-1,-1):
-        for j in range(ord('A'), ord('A') + i + 1):
-            print(chr(j),end="")
-        print("")
+    for i in range(row):
+
+        # space
+        for j in range(row-i-1):
+            print("-",end="")
+
+        # stuff
+        ch = ord('A')
+        brk = ((2*i)+1) / 2
+        for j in range(1, (2*i)+2):
+            print(chr(ch),end="")
+            if j <= brk:
+                ch+=1
+            else:
+                ch-=1
+
+        # space
+        for j in range(row-i-1):
+            print("-",end="")
+        print(" ")
 
 row = 4
-p15(row)
+p17(row)
+
+
+
+# --------------------------------another type ---------------------------------------
+
+# #    A   
+# #   B B  
+# #  C C C 
+# # D D D D
+
+# def p17(row:int)->None:
+
+#     for i in range(row):
+
+#         # space
+#         for j in range(row-i-1):
+#             print("-",end="")
+
+#         # stuff
+#         for j in range((2*i)+1):
+#             ch = ord('A')+i
+#             print(chr(ch),end="")
+
+#         # space
+#         for j in range(row-i-1):
+#             print("-",end="")
+#         print(" ")
+
+# row = 4
+# p17(row)
